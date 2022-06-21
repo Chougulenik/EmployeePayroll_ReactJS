47a54eeb52a63e7ed0d888c17e115af9fbfc9c93
Chougulenik <nikitachougule47@gmail.com>
2022-06-21 09:33:07 +0530

    First Commit: added Employee payroll system using ReactJS

---
+++ employeepayroll/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Components/Header/Header'
+
+export const App = () => {
+  return (
+    <>
+    <Header></Header>
+      <h1>App</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ employeepayroll/src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+export const Header = () => {
+    return (
+        <>
+        <div>
+        <img src="../Assets/Logo/Logo"></img>
+            <div>
+                <h2>
+                    <span className="emp">Employee</span>
+                    <span className="emptext">Payroll</span>
+                 </h2>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Header;
